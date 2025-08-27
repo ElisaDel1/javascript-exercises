81c601a966c2d4992af718c42c90884ce7386676
Elisa <193691473+ElisaDel1@users.noreply.github.com>
2025-08-27 18:39:48 +0200

    Do exercise 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+    let newArray = [];
+
+    array.forEach((item) => {
+        if (!arg.includes(item)) {
+            newArray.push(item);
+        }
+    })
+
+    return newArray;
 };
 
 // Do not edit below this line
