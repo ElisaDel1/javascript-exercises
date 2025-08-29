cdc8f0bad2754fdd39268d9ca0afa8e28ef15d29
Elisa <193691473+ElisaDel1@users.noreply.github.com>
2025-08-29 18:11:06 +0200

    Do exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let numbers = [];
+
+    if (firstNum < 0 || secondNum < 0 || !Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    } else {
+        if (firstNum <= secondNum) {
+            for (let i = firstNum; i <= secondNum; i++) {
+                numbers.push(i);
+            }
+        } else {
+            for (let i = secondNum; i <= firstNum; i++) {
+                numbers.push(i);
+            }
+        }
+
+        return numbers.reduce((acc, number) => acc + number);
+    }
 
 };
 
