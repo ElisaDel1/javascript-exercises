656ad50073746465104fc8c36d93c2a941dd0347
Elisa <193691473+ElisaDel1@users.noreply.github.com>
2025-08-29 19:13:42 +0200

    Do exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahr) {
+  let tempCelcius = ((tempFahr - 32) * (5/9));
+  return +tempCelcius.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelcius) {
+  let tempFahr = (tempCelcius * (9/5) + 32);
+  return +tempFahr.toFixed(1);
 };
 
 // Do not edit below this line
